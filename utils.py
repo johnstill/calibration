7858cebe6f619f85c68361e4ecafd0e355b2013a
John Still <john.m.still@vumc.org>
2018-06-11 14:42:10 -0500

    kde

--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
 import pandas as pd
+from sklearn.metrics import brier_score_loss
 
 
-def load_probs(fname='results.pkl'):
-    """Load up the pickled data and return just the calculated probabilities"""
+def load_results(fname='results.pkl'):
+    """Load up the pickled data and sorts it by brier score loss.  Returns a
+    3-tuple of (predictions, probabilities, actual values).
+    """
     raw = pd.read_pickle(fname)
     actual = raw.actual.copy()
-    probs = raw.xs('Probability', level='Method', axis=1).copy()
-    probs.loc[:, 'actual'] = actual
-    return probs
+    preds = raw.xs('Probability', level='Method', axis=1)
+    probs = raw.xs('Probability', level='Method', axis=1)
+    indices = (probs
+               .apply(lambda v: brier_score_loss(actual, v))
+               .sort_values()
+               .index)
+    preds = preds.reindex(indices, axis=1)
+    probs = probs.reindex(indices, axis=1)
+    return preds, probs, actual
